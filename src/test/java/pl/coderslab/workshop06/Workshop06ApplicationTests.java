13d327fcd718e391e384a76986aa741f6dacf550
Araggorn <zerelik@gmail.com>
2020-11-27 11:16:42 +0100

    aaaa

---
+++ src/test/java/pl/coderslab/workshop06/Workshop06ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.workshop06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshop06ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
